c696e2f3c4f507cc2f8be14bd285f2edaa8729ee
dopAminoNoiR <163659782+dopAminoNoiR@users.noreply.github.com>
2024-03-16 17:47:02 +0100

    Added progress bars
    Progress bars for both, simulation and saving of the animation, were added

--- Wilson_Cowan_Model.py
+++ Wilson_Cowan_Model.py
@@ -2,15 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import itertools
+from tqdm import tqdm
 
 ###########################################################
-random_activity = 0.02 # Spontaneous activity of each neuron
-connect_distance = 3 # Max connectivity distance
-dimensions = [30, 30] # Grid dimensions
-thres = 4 # Activity threshold of each neuron
+random_activity = 0.01  # Spontaneous activity of each neuron
+connect_distance = 3  # Max connectivity distance
+dimensions = [50, 50]  # Grid dimensions
+thres = 4  # Activity threshold of each neuron
 refractory_period = 6  # Refractory period in time steps
 time_steps = 300  # Number of time steps
 ###########################################################
+
 
 class Neuron:
     def __init__(self, random_activity, connect_distance, thres, refractory_period, i, j, dimensions):
@@ -28,7 +30,7 @@ class Neuron:
             x = self.x
         if connect_distance is None:
             connect_distance = self.connect_distance
-            
+
         direct_indices = []
         for a in range(1, int(connect_distance + 1)):
             if x[0] - a < 0 or x[0] + a >= self.dimensions[0]:
@@ -41,11 +43,10 @@ class Neuron:
             direct_indices.append([x[0], x[1] - a])
             direct_indices.append([x[0], x[1] + a])
 
-
         indirect_indices = []
         if int(connect_distance) + 0.5 == connect_distance:
             dist = connect_distance
-        else: 
+        else:
             dist = connect_distance - 1
             for b, c in itertools.product(range(1, int(dist + 1)), repeat=2):
                 if x[0] - b < 0 and x[1] - b < 0 and x[0] - c < 0 and x[1] - c < 0:
@@ -56,10 +57,9 @@ class Neuron:
                 indirect_indices.append([x[0] - b, x[1] + c])
                 indirect_indices.append([x[0] + b, x[1] - c])
                 indirect_indices.append([x[0] + b, x[1] + c])
-                
+
         all_indices = direct_indices + indirect_indices
         return all_indices
-
 
     def update_activity(self, activity_history):
         if self.refractory_time > 0:
@@ -75,33 +75,39 @@ class Neuron:
                 self.activity = np.random.binomial(1, self.random_activity)
 
 
-
-
-
 # Create grid of neurons
 grid = [[Neuron(random_activity, connect_distance, thres, refractory_period, i, j, dimensions) for j in range(dimensions[1])] for i in range(dimensions[0])]
 
 # Simulation loop
 activity_history = np.zeros((time_steps, dimensions[0], dimensions[1]))  # Store activity history
-for t in range(time_steps):
+for t in tqdm(range(time_steps), desc='Simulating'):
     for i in range(dimensions[0]):
         for j in range(dimensions[1]):
             neuron = grid[i][j]
             neuron.update_activity(activity_history)
             activity_history[t, i, j] = neuron.activity
 
-# Visualization
-fig, ax = plt.subplots()
-ims = []
-for t in range(time_steps):
-    im = ax.imshow(activity_history[t], cmap='binary', interpolation='nearest')
-    text = ax.text(0.5, 1.05, 'Time Step: {}'.format(t), transform=ax.transAxes, ha="center")
-    ims.append([im, text])
 
-ani = animation.ArtistAnimation(fig, ims, interval=100, blit=True)
+
+# Visualization
+fig, ax = plt.subplots(dpi = 150)
+im = ax.imshow(activity_history[0], cmap='binary', interpolation='nearest')
+text = ax.text(0.5, 1.05, 'Time Step: 0', transform=ax.transAxes, ha="center")
+
+def update(frame):
+    im.set_array(activity_history[frame])
+    text.set_text('Time Step: {}'.format(frame))
+    return im, text
+
+ani = animation.FuncAnimation(fig, update, frames=time_steps, interval=100, blit=True)
+
+# Calculate total frames
+total_frames = len(list(ani.new_frame_seq()))
 
 # Save animation using PillowWriter
-pillow_writer = animation.PillowWriter(fps= 10)
-ani.save('NewFile.gif', writer=pillow_writer)
-
-plt.show()
+pillow_writer = animation.PillowWriter(fps=15)
+with tqdm(total=total_frames, desc='Saving animation') as pbar:
+    def update_progress(frame, total_frames=total_frames):
+        pbar.update(1)
+        
+    ani.save('NewFile.gif', writer=pillow_writer, progress_callback=update_progress)
